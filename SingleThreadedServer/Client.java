a85715cbe242777a4d8f96aafe1d833345586006
nitinshas3 <shastrin25@gmail.com>
2026-09-12 19:56:01 +0530

    completed single threaded server using sockets input streams output streams etc

---
+++ SingleThreadedServer/Client.java
@@ -0,0 +1,35 @@
+package SingleThreadedServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    public void run() throws IOException {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost"); // whenever server is running on your system it is always called by local host , so this is just getting the ip address of the name local host , basically this is just doing dns on localhost , by this we will get the ip address of the server we want to cnnect
+        System.out.println("Connected to: " + address);
+        Socket socket = new Socket(address,port); // remember socket means ip address plus port number , detailed address of the server , this is that
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream());
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello from the client");
+        toSocket.flush();
+        String line = fromSocket.readLine();
+        System.out.println("Response from the server is " + line);
+
+    }
+
+    public static void main(String[] args) {
+        try{
+            Client client = new Client();
+            client.run();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
---
+++ SingleThreadedServer/Server.java
@@ -0,0 +1,44 @@
+package SingleThreadedServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+
+    public void run() throws IOException {
+        int port = 8010; // this to uniquely identify which server is begin used from the serverside , server side can contain many servers like web servers port no 80 , db servers like postgresql server etc
+        ServerSocket socket = new ServerSocket(port); // serversocket is only for listening to connections(channel) , when actual connection established , the data transfer happens in through socket object
+        //socket.setSoTimeout(1000); // if no request come withing 1 second then breakout of the loop and throw execption
+        while(true){
+            try{
+                System.out.println("SingleThreadedServer.Server is listening on port " + port);
+                Socket acceptedConnections = socket.accept();
+                System.out.println("Connect accepted from client " + acceptedConnections.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnections.getOutputStream()); // this just to print to the client , output input streams just act like pipelines so send receive bytes , this does the actual work of formatting and stuff
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader( acceptedConnections.getInputStream()));
+                // input stream just bytes , inputstream reader converts it to characters , buffer does the readline formating etc , whereas in printing printwriter does both the jobs
+                toClient.println("Hello from the server");
+                toClient.flush();
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try{
+            Server server = new Server();
+            server.run();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
